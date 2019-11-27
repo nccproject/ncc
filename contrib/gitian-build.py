06244f393555e65d9a8695306f2685326ee66ee8
jeongwoo <nc.partners.official@gmail.com>
2019-11-27 18:44:38 +0900

    update gitian:

--- contrib/gitian-build.py
+++ contrib/gitian-build.py
@@ -31,7 +31,7 @@ def setup():
     if not os.path.isdir('ncc'):
         subprocess.check_call(['git', 'clone', 'https://github.com/nccproject/ncc.git'])
     os.chdir('gitian-builder')
-    make_image_prog = ['bin/make-base-vm', '--suite', 'bionic', '--arch', 'amd64']
+    make_image_prog = ['bin/make-base-vm', '--suite', 'bionic', '--arch', 'amd64', '--disksize', '15287']
     if args.docker:
         make_image_prog += ['--docker']
     elif not args.kvm:
